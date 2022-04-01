43db963ff0577d6731c631b5b4a41507ad19b914
rounak-dev <rounak.infymax@gmail.com>
2022-04-01 11:29:06 +0530

    cors and cross-origin added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+
+});
+
 app.get('', (req, res)=>{
     res.send("Hello")
 })
