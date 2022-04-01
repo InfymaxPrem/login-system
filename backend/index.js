c958410c323000ccd4931e841381ed0a446314a1
rounak-dev <rounak.infymax@gmail.com>
2022-04-01 11:16:25 +0530

    express server created

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('', (req, res)=>{
+    res.send("Hello")
+})
+
+app.listen(port, ()=>{
+    console.log(`Your server is running at ${port}`)
+})
